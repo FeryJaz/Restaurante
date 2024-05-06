e69c37c2ae138d74944bde0cb776d9740ff9457d
Fernando Alba <168942512+FeryJaz@users.noreply.github.com>
2024-05-06 13:49:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danzas</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <h1>Restaurante Folkorico Boliviano</h1>
+
+    <header class="header">
+        <div class="menu container">
+            <a  href="#" class="logo">logo</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+                <img src="images/baile 1.jpg" class="menu-icono" alt="menu">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">NOSOTROS</a></li>
+                    <li><a href="#">DANZAS BOLIVIANAS</a></li>
+                    <li><a href="#">PLATOS TIPICOS</a></li>
+                    <li><a href="#">CONTACTO</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section class="header">
+
+        <div class="header-content container">
+            <div class="header-txt">
+                <h2>Iniciativa del Restaurante</h2>
+                <p>
+                    Este proyecto nace para que haya un conocimiento sobre los bailes tipicos de Bolivia en las tres regiones, donde somos diversos en cultura y se dirige a salvaguardar bailes que fueron o dejaran de ser tendencia, para que asi los bolivianos y extranjeros conozcan sobre su diversiad cultural. En esta inicitiva se dara prioridad a danzas menos conocidas por la poblacion. 
+                    Hacer conocer a las personas interesadas sobre la Historia de cada danza boliviana ya que cada una tiene un transfondo fascinante donde algunas de ellas estan envueltas en mitos o leyendas.
+                </p>
+                <a href="*" class="btn-1">Escenario</a>
+            </div>
+            <div class="header-img">
+                <img src="images/escenario.jpg" alt="">
+            </div>
+        </div> 
+
+    </section>
+
+    <section class="about container">
+        <div class="about-img">
+            <ing src="images/historia.webp" alt="">
+        </div>
+        <div class="about txt">
+            <h3>NOSOTROS</h3>
+            <P>
+                VISION
+            </p>
+            <p>
+                El objetivo a futuro se centra en ser un Restaurante reconocido en la ciudad de La Paz por la experincia que brina a cualquier perosna que desee visitar las instalaciones.
+            </P>
+            <p>
+                MISION
+            </p>
+            <P>
+                Inspirar aquellos que tengan ese piritu de nuestras danzas indigenas que son ricas en nuestra cultura y diversidad que tenemos en las tres regiones.
+            </P>
+            <p>
+                VALORES
+            </p>
+            <P>
+                Respeto por la cultura y la diversidad que tiene Boliva.
+            </P>
+        <div class="about container">
+            <img src="images/tobas.jpeg" alt="">
+        </div>
+
+
+
+    </section>
+
+    <section class="about txt">
+        <h3>DANZAS BOLIVIANAS</h3>
+        <P>
+            Hasta el momento 15 danzas bolivianas han sido reconocidas y cuentan con protección de parte de la UNESCO.
+        </P>
+        <p>
+            Las Danzas por Region:
+        </p>
+        <p>
+            Beni Macheteros, Moperas, Danza del Sol y de la Luna, Chovena.
+        </p>
+        <p>
+            Chuquisaca Pujllay, Cueca Chuquisaqueña, Bailecito, Huayño.
+        </p>
+        <p>
+            Cochabamba Cueca, Caporales, El Salaque.
+        </p>
+        <p>
+            La Paz Caporales, Kullawada, Llamerada, Cueca Paceña, Waka Waka, Saya, Incas, Waca Tocoris.
+        </p>
+        <p>
+            Oruro Diablada, Morenada, Kallawayas, Suris Sicuris, Antahuara, Awatiris, Suri Sicuri, Wititis, Sampoñaris y Tarqueada.
+        </p>
+        <p> 
+            Pando Chovena, Taquirari.
+        </p>
+        <p>
+            Potosí Tinku, Potolos y Cueca Potosina.
+        <p>    
+            Santa Cruz Carnavalito, Taquirari, Chovena, Sarao.
+        </p>
+        <p>
+            Tarija Rueda Chapaca, Cueca tarijeña, Chacarera, Escondido, Tonada, Chunchus, Tobas.
+        </p>
+        <div class="about-img">
+            <img src="images/mapa.jpg" alt="">
+            <img src="images/mapa 2.jpg" alt="">
+            <img src="images/mapa 3.jpg" alt="">
+            <img src="images/mapa 4.jpg" alt="">
+        </div>
+
+    </section>
+
+    <main class="PLATOS TIPICOS">
+        <H3>PLATOS TIPICOS</H3>
+        <p>
+            Pique Macho Bs. 40        
+        <div class="about-img">
+            <img src="images/pique macho.jpeg" alt="">
+        </div>
+        <br>
+        <p>
+            Fricase Bs,38
+        <div class="about-img">
+            <img src="images/fricase.jpeg" alt="">
+        </div>
+        <br>
+        <p>
+            Majadito Bs.30
+        <div class="about-img" >
+            <img src="images/MAJADITO.jpeg" alt="">
+        </div>
+        <br>
+        <p>
+            Sopa de mani Bs.25
+        <div class="about-img">
+            <img src="images/sopa de mani1.jpeg" alt="">
+        </div>
+        </p>
+    </main>
+
+    <section class="about txt">
+        <h3>CONTACTO</h3>
+        <P>
+            Direccion
+        </P>
+        <p>
+            Avenida 6 de agosto N 1333
+        </p>
+        <p>
+            Numero de celular
+        </p>
+        <p>
+            7913583
+        </p>
+        <p>
+            Redes Sociales
+        </p>
+        <p>
+            Facebook: Danza_Folkorica-Restaurante 
+            Instagram: Folkore_mucho + 
+            Tik Tok: Danza_Comida:)
+        </p>
+        </div>
+        <br>
+        <div class="about-img">
+           <img src="images/facebook.jpg" alt="">
+        </div>
+
+
+    </section>
+    
+
+    <section class="Agenda de eventos container">
+        <form method="post" autocomplete="off">
+            <h2>Agenda de Eventos</h2>
+            <div class="input-group">
+
+                <div class="input-container">
+                    <input type="text" name="name" placeholder="Nombre y Apellido">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+
+                <div class="input-container">
+                    <input type="tel" name="phone" placeholder="Telefono y Celular">
+                    <i class="fa-solid fa-phone"></i>
+                </div>
+
+                <div class="input-container">
+                    <input type="email" name="email" placeholder="Correo">
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+
+                <div class="input-container">
+                    <textarea name="message" placeholder="Detalles del evento"></textarea>
+                </div>
+                <input type="submit" name="send" class="btn" onClick="myFuction()">
+            </div>
+        </form>
+    </section>
+
+    <footer class="footer">
+        <div class="footer-content container">
+
+            <div class="link">
+                <a href="#" class="logo">logo</a>
+            </div>
+
+            <div class="link">
+                <ul>
+                    <li><a href="#">NOSOTROS</a></li>
+                    <li><a href="#">DANZAS BOLIVIANAS</a></li>
+                    <li><a href="#">PLATOS TIPICOS</a></li>
+                    <li><a href="#">CONTACTO</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <?php
+         include("send.php");
+    
+    ?>
+
+    <script>
+        function myFunction() {
+            window.location.href="http://localhost/Pagina%20Web/"
+        }
+    </script>
+
+</body>
+</html>
